6c7cff835e67f2457650b6238e07ec96e6d09381
valentincognito <valentin.vannay@gmail.com>
2018-10-26 14:01:45 +0900

    Implement filter functions (front)

--- public/js/index.js
+++ public/js/index.js
@@ -4,7 +4,29 @@ $(function () {
 
   let url = new URL(window.location.href)
   let currentPage = Number(url.searchParams.get("page"))
-  let pageCount = Number( $('.pagination .page-count').html() )
+  let pageCount = Number($('.pagination .page-count').html())
+
+  let selectedCategories = url.searchParams.get("categories")
+  let selectedTechnos = url.searchParams.get("technos")
+  let selectedFields = url.searchParams.get("fields")
+  let selectedVisuals = url.searchParams.get("visuals")
+
+  if (selectedCategories) selectedCategories = selectedCategories.split("+")
+  if (selectedTechnos) selectedTechnos = selectedTechnos.split("+")
+  if (selectedFields) selectedFields = selectedFields.split("+")
+  if (selectedVisuals) selectedVisuals = selectedVisuals.split("+")
+
+  let filters = []
+  filters.push(selectedCategories)
+  filters.push(selectedTechnos)
+  filters.push(selectedFields)
+  filters.push(selectedVisuals)
+
+  for (cat of filters) {
+    for (el of cat) {
+      $('.filter-list span:contains('+ el +')').addClass('active')
+    }
+  }
 
   if (!currentPage) currentPage = 1
 
@@ -46,6 +68,44 @@ $(function () {
         //console.log(data.toString());
       }
     })
+  })
+
+  $('.filter-menu li').click(function(){
+    let idx = $('.filter-menu li').index($(this))
+    $('.filter-list').hide()
+    $('.filter-list').eq(idx).toggle()
+  })
+
+  $('.filter-list span').click(function(){
+    $(this).toggleClass('active')
+  })
+
+  $('.navigation .search').click(function(){
+    let categories = []
+    let technos = []
+    let fields = []
+    let visuals = []
+
+    $('.filter-list.category span.active').each(function(index) {
+      categories.push($(this).html())
+    })
+    $('.filter-list.technology span.active').each(function(index) {
+      technos.push($(this).html())
+    })
+    $('.filter-list.field span.active').each(function(index) {
+      fields.push($(this).html())
+    })
+    $('.filter-list.visual span.active').each(function(index) {
+      visuals.push($(this).html())
+    })
+
+    url.searchParams.set("categories", categories.join('+'))
+    url.searchParams.set("technos", technos.join('+'))
+    url.searchParams.set("fields", fields.join('+'))
+    url.searchParams.set("visuals", visuals.join('+'))
+
+    url.searchParams.set("page", 1)
+    location.href = url
   })
 
   $('.pagination .next').click(function(){
